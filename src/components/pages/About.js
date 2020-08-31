22214ebcee73d2e0fc6dc6a16623b43c46d3edb3
SaElBo <sboukaleb@gmail.com>
2020-08-31 17:14:32 +0200

    App funcrionality with functional componenst and Hooks

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React ,{Fragment}from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this App</h1>
+            <p>App to search Githuh Users </p>
+            <p>First react App</p>
+        </Fragment>
+    )
+}
+
+export default About
