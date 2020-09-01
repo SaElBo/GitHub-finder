2dd78d8fa875f5825745b4a9788f2cb985fe5b2d
SaElBo <sboukaleb@gmail.com>
2020-09-01 16:30:56 +0200

    readi for deploiment

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-import PropTypes from "prop-types";
 import GithubContext from "../../context/github/githubContext";
 import AlertContext from "../../context/alert/alertContext";
 
@@ -48,9 +47,5 @@ const Search = () => {
   );
 };
 
-Search.propTypes = {
-
-  setAllert: PropTypes.func.isRequired,
-};
 
 export default Search;
